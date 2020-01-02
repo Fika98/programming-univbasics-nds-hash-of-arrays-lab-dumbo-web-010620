7e1385e5a603d2aaf20de2b5aaa091c9bac89bb0
Firuz Afzalov <afzalov98@gmail.com>
2020-01-02 06:33:24 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -2,9 +2,10 @@ BASE_HOA = {
   :chipmunks => ["Alvin", "Simon", "Theodore"],
   :third_earthers => ["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)"],
   :jetsons => ["George", "Jane", "Judy", "Elroy"]
+  BASE_HOA[:chipmunks] << "Dave"
 }
   BASE_HOA[:chipmunks] << "Dave"
-  return
+  
   
   BASE_HOA[:third_earthers] << "Snarf"
   
